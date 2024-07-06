ea8a7354b417ecc01e9857c13813d17d09a7fe7a
Maxiflexy <onyemax247@gmail.com>
2024-07-06 19:24:44 +0100

    "commit"

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-export default function App(){
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+export default function App() {
+  return <div className="app">
+    <Logo />
+    <Form />
+    <PackingList />
+    <Stats />
+  </div>
 
 }
 
-function Logo(){
-
-  return <h1>Far Away</h1>
-
+function Logo() {
+  return <h1> 🏝 Far Away 🧳</h1>;
 }
 
-function Form(){
-  
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😎 trip?</h3>
+    </div>
+  );
 }
 
-function ParkingList(){
-  
+function PackingList() {
+  return <div className="list">LIST</div>;
 }
 
-function Stats(){
-  
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>💼 You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
 }
